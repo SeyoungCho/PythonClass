c68cb469b0b7a17d6562ec431dcf070d21a22d9a
SeYoung <wnl383@naver.com>
2021-07-28 22:25:00 +0900

    OrderedDictionary

---
+++ chapter6~10/OrderedDictionary.py
@@ -0,0 +1,15 @@
+def sort_by_key(t):
+    return t[0];
+
+from collections import OrderedDict;
+
+d = dict();
+d['l'] = 500; 
+d['x'] = 100;
+d['y'] = 200;
+d['z'] = 300;
+
+for k,v in OrderedDict(sorted(d.items(), key=sort_by_key)).items():
+    print(k, v);
+
+exit();
