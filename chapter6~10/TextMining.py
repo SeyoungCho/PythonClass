fb658b0dd85af46491d15b7571e37186c68e508f
SeYoung <wnl383@naver.com>
2021-08-02 14:53:28 +0900

    마지막

--- chapter6~10/TextMining.py
+++ chapter6~10/TextMining.py
@@ -2,4 +2,20 @@
 # defaultdict 모듈을 사용한다. 
 # 단어의 출현 횟수를 기준으로 정렬된 결과를 보여주기 위하여 OrderedDict모듈을 사용한다. 
 
-from collections import defaultdict, OrderedDict;
+from collections import defaultdict, OrderedDict
+from typing import Counter;
+
+text = """A press release is the quickest and easiest way to get free 
+publicity. If well written, a press release can result in multiple 
+published articles about your firm and its products. 
+And that can mean new prospects contacting you asking 
+you to sell to them.""".lower().split();
+
+def sort_by_key(t):
+    return t[0];
+    
+d = defaultdict(lambda:0);
+d = text;
+c = Counter(d);
+for k,v in OrderedDict(sorted(d.items(), key=sort_by_key)).items():
+    print(k, v);
